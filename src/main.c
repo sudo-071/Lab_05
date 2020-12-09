920833e7dca0739d7963b99f37f8a4fbf6d9f3dd
sudo-071 <ArtemLvov81@gmail.com>
2020-12-09 15:02:27 +0200

    Lab_04

--- src/main.c
+++ src/main.c
@@ -1,49 +1,15 @@
-/**
- * @mainpage
- * # Загальне завдання
- * 1. **Сформувати** функцию, що генерирує структуру із залученням механізму
- * випадкової генерації даних (прикладная галузь вказана в індивідуальному
- * завданні);
- *
- * 2. **Сформувати** функцію, яка буде виводити масив структур на екран
- *
- * @author Davydov V.
- * @date 14-apr-2020
- * @version 1.3
- */
-
-/**
- * @file main.c
- * @brief Файл з демонстрацією роботи структури {@link animal} та методів
- * оперування ним.
- *
- * @author Davydov V.
- * @date 14-apr-2020
- * @version 1.3
- */
-
-#include "lib.h"
-
-/**
- * Головна функція.
- *
- * Послідовність дій:
- * - створення масиву із {@link ANIMAL_COUNT} тварин
- * - генерація даних для кожної тварини шляхом виклика функції
- * {@link generate_animal}
- * - вивід даних про всіх тварин на екран за допомогою функції
- * {@link show_animals}
- * @return успішний код повернення з програми (0)
- */
 int main()
 {
-	srand((unsigned int)time(0));
-	struct animal animals[ANIMAL_COUNT];
-
-	for (unsigned int i = 0; i < ANIMAL_COUNT; i++) {
-		generate_animal(&animals[i]);
+	double src = 123.656;		/*defining the source number*/	
+	int int_part = src;			/*extracting integer part of the number*/		
+	int frac_part = (src - int_part) * 1000;	/*extracting fractional part of the number*/
+	double result;
+	if (int_part != 0){						/*dividing by zero check*/
+		double res = ((float)frac_part / (float)int_part);
+		int res_2 = res * 100;
+		result = res_2 / 100.0;
+	}else{
+		result = 0;/*Couldn`t divide by zero*/
 	}
-	show_animals(animals, ANIMAL_COUNT);
-
 	return 0;
 }
